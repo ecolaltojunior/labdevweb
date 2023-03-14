02cffbe3390f544dcc3024235887d3becd5f498b
ecolaltojunior <edson.colalto@fatec.sp.gov.br>
2023-03-14 10:24:12 -0300

    Rotas

---
+++ 2023-1/aula4/src/pages/Pagina2.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Pagina2 = () => {
+  return (
+    <main>
+        <h2>Segunda página criada com React</h2>
+        <p>Esta é a segunda página criada na aula de Laboratório de Desenvolvimento Web, 4º semestre de DSM - Manhã</p>
+
+    </main>
+  )
+}
+
+export default Pagina2
---
+++ 2023-1/aula4/src/pages/Pagina3.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Pagina3 = () => {
+  return (
+    <main>
+        <h2>Terceira página</h2>
+        <p>Fiquei com preguiça, mas essa é a terceira página.</p>
+    </main>
+  )
+}
+
+export default Pagina3
