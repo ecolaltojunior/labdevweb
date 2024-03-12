19f87b0cd081d90af389d097504d0d523a4bb216
ecolaltojunior <edson.colalto@fatec.sp.gov.br>
2024-03-12 10:08:09 -0300

    ex3 - props

---
+++ 2024-1/props/src/components/ShowUserName.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowUserName = (props) => {
+  return (
+    <div>
+        <h2>O nome do usuário é: {props.name}</h2>
+    </div>
+  )
+}
+
+export default ShowUserName
