c81e1ab83da1e379ad2ddbd04a6c874a2a09c35e
Edson Company Colalto Junior <84912338+ecolaltojunior@users.noreply.github.com>
2024-03-26 14:04:54 +0000

    manipulando estado em formulário

--- 2024-1/ex4/src/pages/Form.jsx
+++ 2024-1/ex4/src/pages/Form.jsx
@@ -1,20 +1,35 @@
 import React from 'react'
 import './Form.css'
+import { useState } from 'react'
+
 const Form = () => {
+    const [nome, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [mensagem, setMessage] = useState("")
+
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        console.log("Enviando formulário!")
+        console.log(nome, email, mensagem)
+
+        setName("")
+        setEmail("")
+        setMessage("")
+    }
   return (
     <div>
-        <form>
+        <form onSubmit={handleSubmit}>
             <label>
                 <span>Nome</span>
-                <input type="text" name="nome" id="nome" placeholder="Nome completo" />
+                <input type="text" name="nome" id="nome" placeholder="Nome completo" onChange={(e)=>setName(e.target.value)} value={nome}/>
             </label>
             <label>
                 <span>E-Mail</span>
-                <input type="email" name="email" id="email" placeholder='nome@site.com' />
+                <input type="email" name="email" id="email" placeholder='nome@site.com' onChange={(e)=>setEmail(e.target.value)} value={email} />
             </label>
             <label>
                 <span>Mensagem</span>
-                <textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
+                <textarea name="mensagem" id="mensagem" cols="30" rows="10" onChange={(e)=>setMessage(e.target.value)} value={mensagem}></textarea>
             </label>
             <button type="submit">Enviar</button>
         </form>
