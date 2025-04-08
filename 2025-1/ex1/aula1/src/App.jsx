828b37ff32a7886de385807fe2c886c99b65d2d6
ecolaltojunior <edson.colalto@fatec.sp.gov.br>
2025-04-08 08:22:45 -0300

    ex1

---
+++ 2025-1/ex1/aula1/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Form from './pages/Form'
+
+function App() {
+  
+
+  return (
+    <>
+      <Form />
+    </>
+  )
+}
+
+export default App
---
+++ 2025-1/ex1/aula1/src/pages/Form.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+    <main>
+        <h1>Formulário de contato</h1>
+        <form>
+            <fieldset>
+                <legend>Dados da mensagem</legend>
+            <label>
+                <span>Nome</span>
+                <input type="text" name="nome" id="nome" placeholder='Nome completo' />
+            </label><br />
+            <label>
+                <span>Assunto</span>
+                <input type="text" name="assunto" id="assunto" placeholder='informe o assunto da mensagem'/>
+            </label><br />
+            <label>
+                <span>Mensagem</span>
+                <textarea name="mensagem" id="mensagem" cols="30" rows="10"></textarea>
+            </label>
+            </fieldset>
+            <button type='submit'>Enviar</button>
+        </form>
+    </main>
+  )
+}
+
+export default Form
